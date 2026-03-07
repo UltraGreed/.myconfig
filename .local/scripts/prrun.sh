ca0f7b7243aed1a0d2d6d5bb4b977e9d13f54b43
ultragreed <agent002004@gmail.com>
2026-03-07 19:02:29 +1000

    smth

--- .local/scripts/prrun.sh
+++ .local/scripts/prrun.sh
@@ -2,4 +2,4 @@ export STEAM_COMPAT_DATA_PATH="/home/ultragreed/.wine/"
 export STEAM_COMPAT_CLIENT_INSTALL_PATH="~/.steam/steam"
 export WINE_SIMULATE_WRITECOPY=1
 export WINEDLLOVERRIDES="locationapi=d"
-~/.steam/steam/compatibilitytools.d/GE-Proton8-27/proton run $1
+/home/ultragreed/.steam/steam/compatibilitytools.d/GE-Proton10-9/proton run $1
